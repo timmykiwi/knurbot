13c2f850173c9c3f443add08657b83499a1184bf
timmykiwi <57061878+timmykiwi@users.noreply.github.com>
2020-11-19 13:14:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,3 +16,30 @@ config({
     require(`./handler/${handler}`)(client);
 });
 
+client.on("ready", async () => {
+  console.log('${client.user.username} jest online!')
+  client.user.setGame("")
+  client.user.setStatus('dnd')
+});
+
+client.on("message", async message => {
+    const prefix = "/";
+
+    if (message.author.bot) return;
+    if (!message.guild) return;
+    if (!message.content.startsWith(prefix)) return;
+    if (!message.member) message.member = await message.guild.fetchMember(message);
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+
+    if (cmd.length === 0) return;
+
+    let command = client.commands.get(cmd);
+    if (!command) command = client.commands.get(client.aliases.get(cmd));
+
+    if (command)
+        command.run(client, message, args);
+});
+
+client.login(process.env.TOKEN);
